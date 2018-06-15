aca5c5192fd1995aaedd242a5590fcbcaa9d282a
Carter, Derek <derek.carter@pearson.com>
2018-06-14 20:11:15 -0400

    tyring to get the package.json on the container

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ sudo npm -v
 sudo mkdir -p /app
 sudo cp -rf scripts /app/scripts
 sudo cp -rf src /app/src
-sudo cp *.json /app
-sudo cp nginx.conf /app
+sudo cp *.json /app/
+sudo cp nginx.conf /app/
 sudo /usr/local/bin/fpm -s dir -n dev-portal-ui -v 0.1.0 --iteration $(date "+%Y%m%d%H%M%S") -t deb /app
