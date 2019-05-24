25cbc853d11dae46dc29bbad311d78c4c209a318
Carter, Derek <derek.carter@pearson.com>
2019-05-23 21:12:43 -0400

    keep persistent state in tact

--- src/app/documentation/documentation-side-navigation/documentation-area/documentation-area.component.ts
+++ src/app/documentation/documentation-side-navigation/documentation-area/documentation-area.component.ts
@@ -12,6 +12,8 @@ export class DocumentationAreaComponent implements OnInit {
 
   @Input() documentationAreas: DocumentationArea[];
   @Input() defaultAllowedDepth: number = 2;
+  @Input() state: any = {test : false};
+
   @Output() documentationAreaClick: EventEmitter<string> = new EventEmitter<string>();
   @Output() addDocumentClick: EventEmitter<string> = new EventEmitter<string>();
   public depth: number = 0;
@@ -20,6 +22,10 @@ export class DocumentationAreaComponent implements OnInit {
 
   ngOnInit() {
     this.depth = (this.documentationAreas && this.documentationAreas.length) ? this.getDepth(this.documentationAreas[0]) : this.depth;
+  }
+
+  public updateState(documentationArea: DocumentationArea) {
+    setTimeout(t=> {this.state[documentationArea.id] = !this.state[documentationArea.id];}, 0);
   }
 
   public getDepth (documentationArea: DocumentationArea) : number{
@@ -38,7 +44,11 @@ export class DocumentationAreaComponent implements OnInit {
     this.documentationAreaClick.emit(id);
   }
 
-  public emitAddDocument(id: string) {
+  public emitAddDocument(id: string, event) {
+    if(event){
+      event.preventDefault();
+      event.stopPropagation();
+    }
     this.addDocumentClick.emit(id);
   }
 }
--- src/app/documentation/documentation-side-navigation/documentation-side-navigation.component.ts
+++ src/app/documentation/documentation-side-navigation/documentation-side-navigation.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { BlogPost } from '../interfaces/blog-post.interface';
 import { DocumentationArea } from '../../core/interfaces/documentation-area.interface';
-import { Documentation } from '../../core/interfaces/documentation.interface';
 
 @Component({
   selector: 'documentation-side-navigation',
@@ -13,101 +12,13 @@ import { Documentation } from '../../core/interfaces/documentation.interface';
 })
 export class DocumentationSideNavigationComponent implements OnInit {
 
-  @Input() documentationBlogs: BlogPost[];
   @Input() documentationAreas: DocumentationArea[];
   @Input() currentId: string;
   @Input() currentSlug: string;
-  @Output() blogClick: EventEmitter<BlogPost> = new EventEmitter<BlogPost>();
+  @Input() defaultAllowedDepth: number = 2;
 
+  @Output() blogClick: EventEmitter<BlogPost> = new EventEmitter<BlogPost>();
   public collapsdeState = {};
-  public dummyDocumentationAreas: DocumentationArea[] = [
-    {
-      id : 'asdf11',
-      name : 'Test1',
-      description : 'Description1',
-      slug : 'test1',
-      position : 1,
-      parentSlug : null,
-      documents : [
-        <Documentation> {
-          id : 'asdf1114',
-          name : 'Documetation3',
-          description : "Description",
-          parentSlug : 'test1',
-          content : '<b>this is a test </b>'
-        },
-        <Documentation> {
-          id : 'asdf1115',
-          name : 'Documetation4',
-          description : "Description",
-          parentSlug : 'test1',
-          content : '<b>this is a test </b>'
-        }
-      ],
-      children : [
-        <DocumentationArea> {
-          id : 'asdf1122',
-          name : 'TEST 10',
-          description : 'TEST 10 Description',
-          slug : 'test10',
-          position : 1,
-          children : [],
-          documents : [
-            <Documentation> {
-              id : 'asdf1123',
-              name : 'Documetation11',
-              description : "Description",
-              parentSlug : 'test/test10',
-              content : '<b>this is a test </b>'
-            }
-          ]
-        },
-        <DocumentationArea> {
-          id : 'asdf12',
-          name : 'Test2',
-          description : 'Description2',
-          slug : 'test2',
-          position : 1,
-          parentSlug : 'asdf11',
-          children : [
-            <DocumentationArea> {
-              id : 'asdf17',
-              name : 'Test4',
-              description : 'Description4',
-              slug : 'test2',
-              position : 1,
-              parent : 'asdf12',
-              documents : [
-                <Documentation> {
-                  id : 'asdf1116',
-                  name : 'Documetation6',
-                  description : "Description",
-                  parentSlug : 'test1/test2',
-                  content : '<b>this is a test </b>'
-                }
-              ]
-            }
-          ],
-          documents : [
-            <Documentation> {
-              id : 'asdf1113',
-              name : 'Documetation1',
-              description : "Description",
-              parentSlug : 'test1/test2',
-              content : '<b>this is a test </b>'
-            },
-            <Documentation> {
-              id : 'asdf1112',
-              name : 'Documetation2',
-              description : "Description",
-              parentSlug : 'test1/test2',
-              content : '<b>this is a test </b>'
-            }
-          ]
-        }
-      ]
-    }
-  ]
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -116,16 +27,40 @@ export class DocumentationSideNavigationComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.setInitialState(this.documentationAreas);
+
     this.router.events.subscribe( (event:NavigationEnd) => {
       if( this.router['lastSuccessfulId'] === this.router['navigationId'] ){
         if(this.activatedRoute.children.length && this.activatedRoute.children[0].children.length){
           this.activatedRoute.children[0].children[0].params.subscribe(p => {
             this.currentSlug = p.slug;
+            console.log("HERE");
           });
         }
         if(this.activatedRoute.children.length && this.activatedRoute.children[0].snapshot.data.LandingPage){
           this.currentSlug = null;
         }
+      }
+    });
+  }
+
+  public setInitialState (documentationAreas: DocumentationArea[]) {
+    this.traverseTreeAndSetState(documentationAreas);
+  }
+
+  public traverseTreeAndSetState (documentationAreas: DocumentationArea[]) {
+    documentationAreas.forEach(da => {
+
+      let depth = da.slug.split("/").length;
+
+      this.collapsdeState[da.id] = false;
+
+      if(depth >= this.defaultAllowedDepth)
+        this.collapsdeState[da.id] = true;
+      
+
+      if(da.children && da.children.length){
+        this.traverseTreeAndSetState(da.children);
       }
     });
   }
